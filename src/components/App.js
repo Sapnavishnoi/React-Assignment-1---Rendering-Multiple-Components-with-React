fe8302dd1f67f14acd5625de35623acab14cb98e
Sapnavishnoi <sapna18@navgurukul.org>
2021-02-11 19:49:59 +0530

    Rendring multiple component with react

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="root">
+      <h1 data-ns-test= "project-name">
+      project-name
+      </h1>
+      <p data-ns-test= "project-name">project description</p>
+    </div>
   )
 }
 
